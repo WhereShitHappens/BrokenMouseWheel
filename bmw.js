fbc447a30aa6f085c34cb5a16fb8261b86abeaec
Aris <zer0xher0@hotmail.com>
2017-09-10 17:29:24 +0300

    Add files via upload

---
+++ bmw.js
@@ -0,0 +1,13 @@
+
+var height = document.documentElement.scrollHeight;
+
+function scroll(event){
+    var y = event.clientY;
+    var yPercentage = y/screen.height;
+    //ctrlKey is subject to change as more features will be added
+    if (event.ctrlKey)
+    window.scrollTo(0,yPercentage*height);
+}
+
+
+window.onmousemove = scroll;
